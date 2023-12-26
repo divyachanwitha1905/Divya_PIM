c5d99d73001354284a00d9b724f22141f2030cde
mandala divya kumari <divyakumari609@gmail.com>
2023-12-26 14:29:35 +0530

    commit all changes

--- divya.py
+++ divya.py
@@ -85,8 +85,8 @@ def predict(image):
     detections = []
     for output in results:
         if output is not None:
-            for detection in output:
-                x1, y1, x2, y2, obj_conf, class_conf, class_id = detection.tolist()
+            for detection in output.boxes.tolist():  # Convert boxes tensor to list
+                x1, y1, x2, y2, obj_conf, class_conf, class_id = detection
                 detections.append({
                     'xmin': x1,
                     'ymin': y1,
@@ -109,10 +109,9 @@ def predict(image):
 
     image_with_boxes = draw_polygons(image, detections_df)
     return len(detections_df), detections_df, image_with_boxes
+DataFrame(detections)
 
-
-    detections_df = pd.DataFrame(detections)
-
+    
     # Apply confidence threshold
     detections_df = detections_df[detections_df['confidence'] > 0.5]
 
